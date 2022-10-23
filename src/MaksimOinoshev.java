1e2e189b4b812bb60710d799d893a927b3da3528
m.oynoshev <m.oynoshev@tinkoff.ru>
2022-10-23 21:37:51 +0300

    Add documentaion

--- src/MaksimOinoshev.java
+++ src/MaksimOinoshev.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,79 +9,69 @@ import java.util.regex.Pattern;
 /**
  * @author Maksim Oinoshev
  */
-public class MaksimOinoshev{
+public class MaksimOinoshev {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         var coordinates = parseCoordinates(scanner.nextLine());
-        var game_mode = parseGameMode(scanner.nextLine());
+        var gamemode = parseGameMode(scanner.nextLine());
         scanner.close();
-        
-        final char[][] jackObserve;
-        if (game_mode == 1) {
-            jackObserve = new char[][]{
-                {'#', '#', '#'},
-                {'#', '#', '#'},
-                {'#', '#', '#'},
-            };
-        } else {
-            jackObserve = new char[][]{
-                {'-', '-', '#', '-', '-'},
-                {'-', '#', '#', '#', '-'},
-                {'#', '#', '#', '#', '#'},
-                {'-', '#', '#', '#', '-'},
-                {'-', '-', '#', '-', '-'},
-
-            };
-        }
 
         final char[][] krakenAreaEffect = {
-            {'-', '#', '-'},
-            {'#', '#', '#'},
-            {'-', '#', '-'},
+                { '-', '#', '-' },
+                { '#', '#', '#' },
+                { '-', '#', '-' },
         };
 
         final char[][] davyAreaEffect = {
-            {'#', '#', '#'},
-            {'#', '#', '#'},
-            {'#', '#', '#'},
+                { '#', '#', '#' },
+                { '#', '#', '#' },
+                { '#', '#', '#' },
         };
-        
-        var jack = new Player(coordinates.get(0), "jack", jackObserve);
+
+        var jack = new Player(coordinates.get(0), "jack");
         var davy = new Enemy(coordinates.get(1), "davy", davyAreaEffect);
         var kraken = new Enemy(coordinates.get(2), "kraken", krakenAreaEffect, false);
         var stone = new Enemy(coordinates.get(3), "stone");
         var treasure = new Goal(coordinates.get(4), "brilliant");
         var tartuga = new Support(coordinates.get(5), "tartuga");
 
-        var map = new TreasureMap(treasure, jack, tartuga, new Enemy[]{kraken, davy, stone});
+        var map = new TreasureMap(gamemode, treasure, jack, tartuga, new Enemy[] { kraken, davy, stone });
         map.aStar();
     }
 
+    /**
+     * Call InvalidInput if input does not equal to 1 or 2
+     * 
+     * @return game mode
+     */
     public static int parseGameMode(String input) {
         var mode = -1;
         try {
             mode = Integer.parseInt(input);
+            if (mode == 1 || mode == 2)
+                return mode;
         } catch (Exception e) {
             InvalidInput();
         }
-        
-        if (mode == 1 || mode == 2)
-            return mode;
-            
+
         InvalidInput();
         return 0;
     }
 
+    /**
+     * Translate the input string to the list of arrays format, where
+     * each array contain pair of (y, x) coordinates
+     */
     public static ArrayList<int[]> parseCoordinates(String input) {
         Pattern pattern = Pattern.compile("(\\[\\d,\\d\\]{1} ){5}\\[\\d,\\d\\]{1}$");
         Matcher matcher = pattern.matcher(input);
-        if (!matcher.find()) 
+        if (!matcher.find())
             InvalidInput();
 
         // divide line to chunks
         var strCoordinates = input.split(" ");
         var coordinates = new ArrayList<int[]>();
-    
+
         for (String strCoordinate : strCoordinates) {
             // remove square braces and get int array by comma
             var coordinate = Arrays.stream(strCoordinate.replaceAll("[\\[\\]]", "")
@@ -94,18 +82,21 @@ public class MaksimOinoshev{
         return coordinates;
     }
 
+    /**
+     * Finish the program with invalid input message
+     */
     public static void InvalidInput() {
         System.out.println("Invalid input");
         System.exit(0);
     }
 }
 
-
 class TreasureMap {
     private final int emptyCellCost = 1;
     private final int enemyCellCost = -1;
     private final int width = 9;
     private final int height = 9;
+    private final int gamemode;
 
     private final Goal goal;
     private final Player player;
@@ -114,18 +105,18 @@ class TreasureMap {
 
     private final Cell[][] body = new Cell[width][height];
 
-    public TreasureMap(Goal goal, Player player, Support support, Enemy[] enemies) {
+    public TreasureMap(int gamemode, Goal goal, Player player, Support support, Enemy[] enemies) {
         this.goal = goal;
         this.player = player;
         this.support = support;
         this.enemies = enemies;
-        
+        this.gamemode = gamemode;
+
         placeEnemiesOnMap(enemies);
         fillMapWithEmptyCells();
     }
 
     public void aStar() {
-        printMap();
         var initCell = getEntityCell(player);
         var deadCells = new HashSet<Cell>();
         var cheapestCells = new ArrayList<Cell>();
@@ -139,23 +130,23 @@ class TreasureMap {
             if (cellContains(cell, goal)) {
                 goalCell = cell;
                 break;
-            };
+            }
 
             var neighborCells = getNeighborCells(cell);
             if (neighborCells.isEmpty()) {
                 deadCells.add(cell);
                 continue;
             }
-            
+
             deduplicate(neighborCells, cheapestCells, proceededCells, deadCells);
-            
+
             recalculateTotalCost(cell, neighborCells);
 
             cheapestCells.addAll(neighborCells);
             cheapestCells.sort(Comparator.comparing(Cell::getTotalConst));
             proceededCells.add(cell);
         }
-        
+
         if (goalCell != null) {
             System.out.println("WIN!");
             markWinPath(goalCell);
@@ -164,7 +155,16 @@ class TreasureMap {
             System.out.println("NO WIN!");
     }
 
-    private void deduplicate(ArrayList<Cell> neighborCells, ArrayList<Cell> cheapestCells, ArrayList<Cell> procededCells, HashSet<Cell> deadCells) {
+    /**
+     * Removes duplicate element from the neighborCells and cheapestCells lists
+     * 
+     * @param neighborCells - array list of neighbors
+     * @param cheapestCells - array list of cell sorted by total cost
+     * @param procededCells - array list of proceded cells from cheapestCells
+     * @param deadCells     - set of cells without neighbors
+     */
+    private void deduplicate(ArrayList<Cell> neighborCells, ArrayList<Cell> cheapestCells,
+            ArrayList<Cell> procededCells, HashSet<Cell> deadCells) {
         var cheapestCellsToRemove = new ArrayList<Cell>();
         var neighborCellsToRemove = new ArrayList<Cell>();
 
@@ -183,18 +183,21 @@ class TreasureMap {
                     neighborCellsToRemove.add(neighbor);
 
             // remove collision of dead and neighbor cells
-            for (Cell dead : deadCells) 
-                if (dead.id == neighbor.id) 
+            for (Cell dead : deadCells)
+                if (dead.id == neighbor.id)
                     neighborCellsToRemove.add(neighbor);
         }
 
         for (Cell cellToRemove : neighborCellsToRemove)
             neighborCells.remove(cellToRemove);
 
-        for (Cell cellToRemove : cheapestCellsToRemove) 
+        for (Cell cellToRemove : cheapestCellsToRemove)
             cheapestCells.remove(cellToRemove);
     }
 
+    /**
+     * Sets isWiningPath to true for the shortest path
+     */
     private void markWinPath(Cell goalCell) {
         var cell = goalCell;
         var initCell = getEntityCell(player);
@@ -204,16 +207,20 @@ class TreasureMap {
         }
     }
 
+    /**
+     * Display map to stdout
+     */
     private void printMap() {
         var entities = new ArrayList<Entity>(Arrays.asList(player, goal, support));
         entities.addAll(Arrays.asList(enemies));
+
         System.out.println(" 012345678");
-        for(int i = 0; i < height; i++) {
+        for (int i = 0; i < height; i++) {
             System.out.print(i);
 
-            for(int j = 0; j < width; j++) {
+            for (int j = 0; j < width; j++) {
                 var cell = body[i][j];
-                
+
                 var isEntity = false;
                 for (Entity entity : entities) {
                     if (cell == getEntityCell(entity)) {
@@ -222,7 +229,8 @@ class TreasureMap {
                         break;
                     }
                 }
-                if (isEntity) continue;
+                if (isEntity)
+                    continue;
 
                 if (cell.cost == enemyCellCost) {
                     System.out.print(Colors.RED + "#" + Colors.STOP);
@@ -239,43 +247,51 @@ class TreasureMap {
         }
     }
 
+    /**
+     * Returns neighbors of the cell. A neighbor is cell which can be accepted
+     * from the given cell by one move
+     * 
+     * @param cell
+     * @return neighbors of the cell
+     */
     private ArrayList<Cell> getNeighborCells(Cell cell) {
         int x = cell.x;
         int y = cell.y;
 
         var neighbors = new ArrayList<Cell>();
-        var observeArea = player.getObserveArea();
-        int heightShiftInArea = observeArea.length / 2;
-        int WidthShiftInArea = observeArea[0].length / 2;
 
-        for(int i = -heightShiftInArea; i < heightShiftInArea + 1; i++) {
-            for(int j = -WidthShiftInArea; j < WidthShiftInArea + 1; j++) {
-                if (i == 0 && j == 0) continue;  //cell itself
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (i == 0 && j == 0)
+                    continue; // cell itself
 
                 int yMap = y + i;
                 int xMap = x + j;
 
-                if (inMap(yMap, xMap) && !inEffectArea(yMap, xMap)){
-                    var neighbor = body[yMap][xMap];
-                    int yArea = i + heightShiftInArea;
-                    int xArea = j + WidthShiftInArea;
-                    
-                    if (cell.parent != neighbor && observeArea[yArea][xArea] == '#') {
-                        var neighborCopy = neighbor.copy();
-                        neighborCopy.parent = cell;
-                        neighbors.add(neighborCopy);
-                    }
+                if (inMap(yMap, xMap) && !inEffectArea(yMap, xMap)) {
+                    var neighbor = body[yMap][xMap].copy();
+                    neighbor.parent = cell;
+                    neighbors.add(neighbor);
                 }
             }
         }
         return neighbors;
     }
 
+    /**
+     * Returns cell that contains the given entity
+     */
     private Cell getEntityCell(Entity entity) {
         return body[entity.getY()][entity.getX()];
     }
 
-    private void recalculateTotalCost(Cell cell, ArrayList<Cell> neighbors){
+    /**
+     * Assign new costs for the neighbor if go from the cell to the neighbor
+     * 
+     * @param cell      - cell to go from
+     * @param neighbors - list of neighbors of the cell
+     */
+    private void recalculateTotalCost(Cell cell, ArrayList<Cell> neighbors) {
         for (Cell neighbor : neighbors) {
             int[] costs = calculateCost(neighbor);
             int newCost = costs[0];
@@ -288,23 +304,35 @@ class TreasureMap {
         }
     }
 
+    /**
+     * Calculates new costs for the cell
+     * 
+     * @return the array of new self cost, manhatan cost and total cost
+     */
     private int[] calculateCost(Cell cell) {
         int cathetus_x = Math.abs(goal.getX() - cell.x);
         int cathetus_y = Math.abs(goal.getY() - cell.y);
         int manhatanCost = Math.max(cathetus_x, cathetus_y);
 
         int newCost = cell.cost;
-        if (cell.parent != null) newCost += cell.parent.cost;
+        if (cell.parent != null)
+            newCost += cell.parent.cost;
 
         int newTotalCost = newCost + manhatanCost;
-        
-        return new int[] {newCost, manhatanCost, newTotalCost};
+
+        return new int[] { newCost, manhatanCost, newTotalCost };
     }
 
+    /**
+     * Check if (x, y) in the map
+     */
     private boolean inMap(int y, int x) {
         return xInMap(x) && yInMap(y);
     }
 
+    /**
+     * Check if (x, y) in the enemy effect area
+     */
     private boolean inEffectArea(int y, int x) {
         return body[y][x].cost == -1;
     }
@@ -317,16 +345,16 @@ class TreasureMap {
         return y < width && y >= 0;
     }
 
+    /**
+     * Check if the given cell contains the given entity
+     */
     private boolean cellContains(Cell cell, Entity entity) {
         return cell.x == entity.getX() && cell.y == entity.getY();
     }
 
-    private void placeSupportOnMap(Support support) {
-        int x = support.getX();
-        int y = support.getY();
-        body[y][x] = new Cell(enemyCellCost, y, x);
-    }
-    
+    /**
+     * Sets enemyCellCost for enemies and it's effect area cells
+     */
     private void placeEnemiesOnMap(Enemy[] enemies) {
         for (Enemy enemy : enemies) {
             int x = enemy.getX();
@@ -335,7 +363,10 @@ class TreasureMap {
             placeEffectAreaOnMap(enemy);
         }
     }
-    
+
+    /**
+     * Sets enemyCellCost for enemies effect area cells
+     */
     private void placeEffectAreaOnMap(Enemy enemy) {
         var area = enemy.getEffectArea();
         if (area == null)
@@ -348,12 +379,12 @@ class TreasureMap {
         var enemy_pos_y_in_area = area_height / 2;
         var enemy_pos_x_in_area = area_width / 2;
 
-        for(int i = 0; i < area_height; i++) {
-            for(int j = 0; j < area_width; j++) {
+        for (int i = 0; i < area_height; i++) {
+            for (int j = 0; j < area_width; j++) {
                 if (area[i][j] == '#') {
                     int dis_x = j - enemy_pos_x_in_area;
                     int dis_y = i - enemy_pos_y_in_area;
-                    
+
                     int effect_pos_x = enemy.getX() + dis_x;
                     int effect_pos_y = enemy.getY() + dis_y;
 
@@ -364,16 +395,19 @@ class TreasureMap {
         }
     }
 
+    /**
+     * Put cells with emptyCellCost in the map body
+     */
     private void fillMapWithEmptyCells() {
-        for(int i  = 0; i < height; i++) {
-            for(int j = 0; j < width; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 if (body[i][j] == null)
                     body[i][j] = new Cell(emptyCellCost, i, j);
             }
         }
     }
-    
-    class Cell{
+
+    class Cell {
         private int id;
         private int x;
         private int y;
@@ -386,15 +420,17 @@ class TreasureMap {
         private int totalCost = -1;
 
         private Cell parent = null;
-        
+
         public Cell(int cost, int y, int x) {
             this.cost = cost;
             this.x = x;
             this.y = y;
-            this.id = (x + 1) + (y) * 9 ;
+            this.id = (x + 1) + (y) * 9;
         }
-        
-        public Cell(int id, int x, int y, boolean isSupportPoint, boolean isWinPath, int cost, int manhatanCost, int totalCost, Cell parent) {
+
+        public Cell(int id, int x, int y, boolean isSupportPoint,
+                boolean isWinPath, int cost, int manhatanCost,
+                int totalCost, Cell parent) {
             this.id = id;
             this.x = x;
             this.y = y;
@@ -416,26 +452,17 @@ class TreasureMap {
 
         @Override
         public String toString() {
-            return "id:" + id + "," + "y:" + y + "," + "x:" + x ;
+            return "id:" + id + "," + "y:" + y + "," + "x:" + x;
         }
 
-        
     }
 }
 
 class Player extends Entity {
-    private char[][] observeArea;
-
-    public Player(int[] coordinates, String name, char[][] observeArea) {
+    public Player(int[] coordinates, String name) {
         super(coordinates, name);
-        this.observeArea = observeArea;
     }
-
-    public char[][] getObserveArea() {
-        return observeArea;
-    } 
 }
-
 
 class Enemy extends Entity {
     private char[][] effectArea = null;
@@ -445,7 +472,7 @@ class Enemy extends Entity {
         super(coordinates, name);
     }
 
-    public Enemy(int[] coordinates, String name, char[][]effectArea) {
+    public Enemy(int[] coordinates, String name, char[][] effectArea) {
         super(coordinates, name);
         this.effectArea = effectArea;
     }
@@ -461,20 +488,17 @@ class Enemy extends Entity {
     }
 }
 
-
 class Support extends Entity {
     public Support(int[] coordinates, String name) {
         super(coordinates, name);
     }
 }
 
-
 class Goal extends Entity {
     public Goal(int[] coordinates, String name) {
         super(coordinates, name);
     }
 }
-
 
 class Entity {
     private int x;
@@ -509,7 +533,7 @@ enum Colors {
     STOP("\033[0m"),
     RED("\033[0;31m"),
     GREEN("\033[0;32m"),
-    YELLOW("\033[0;33m"); 
+    YELLOW("\033[0;33m");
 
     private String code;
 
